0c21a30ed69f2a108a1a293ec0aa0b59751109fb
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-01 18:48:28 +0000

    Creating Basic single thread Server Added new features: Ability for feedback

--- src/Server.java
+++ src/Server.java
@@ -1,15 +1,38 @@
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        // Create Server
+        // === Establish server === //
+
+        // Create Server Socket
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        // Start Listening
-        serverSocket.accept();
+        // Create Client Socket
+        Socket clientSocket = serverSocket.accept();
+
+        // === Create feedback === //
+
+        // Version 1
+        clientSocket.getOutputStream().write("<H2>Hello</H2>".getBytes());
+
+        // Version 2
+        OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+        out.write("<H2>From</H2>");
+        out.write("<H2>Server</H2>");
+        out.flush();
+
+        // === Close All Connections === //
+
+        // Close OutputStreamWriter
+        out.close();
+
+        // Close Client socket
+        clientSocket.close();
 
         // Close Server
         serverSocket.close();
