afc35388b6188a839fbaa994b037c67affef7b4d
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-02 00:08:57 +0000

    Creating Basic single thread Server Added new features: Allowing for 5 connections

--- src/Server.java
+++ src/Server.java
@@ -7,35 +7,48 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
+        // === initialize variables === //
+
+        // Counter
+        int count = 0;
+
         // === Establish server === //
 
         // Create Server Socket
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        // Create Client Socket
-        Socket clientSocket = serverSocket.accept();
+        // 5 connections
+        while (count < 5) {
+            // Create Client Socket
+            Socket clientSocket = serverSocket.accept();
 
-        // === Create feedback === //
+            // Counting how many clients reached the server
+            System.out.println("Connection no.:\t" + ++count);
 
-        // Version 1
-        clientSocket.getOutputStream().write("<H2>Hello</H2>".getBytes());
+            // === Create feedback === //
 
-        // Version 2
-        OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
-        out.write("<H2>From</H2>");
-        out.write("<H2>Server</H2>");
-        out.flush();
+            // Version 1
+            clientSocket.getOutputStream().write("<H2>Hello</H2>".getBytes());
 
-        // === Close All Connections === //
+            // Version 2
+            OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+            out.write("<H2>From</H2>");
+            out.write("<H2>Server</H2>");
+            out.flush();
 
-        // Close OutputStreamWriter
-        out.close();
+            // === Close All Connections === //
 
-        // Close Client socket
-        clientSocket.close();
+            // Close OutputStreamWriter
+            out.close();
 
-        // Close Server
-        serverSocket.close();
+            // Close Client socket
+            clientSocket.close();
+        }
+
+        if (!serverSocket.isClosed()) {
+            serverSocket.close();
+        }
+
 
     }
 }
