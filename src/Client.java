a8f1026e502f88e2b8edf1b5dc183c4a8a440283
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-03 14:23:01 +0000

    Creating Basic single thread Server Added new features: The client sends a message to the server, and get return message from the server

--- src/Client.java
+++ src/Client.java
@@ -1,31 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+
         // Create client socket
         Socket clientSocket = new Socket("127.0.0.1", 8000);
 
+
+        // === Send data to Server === //
+
+
+        // Send message to server
+        OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+        out.write("Hello, I am client\n");
+        out.flush();
+
+
         // === Read data from server === //
 
-        // Version 1 read one byte(1 symbol)
-        System.out.println(clientSocket.getInputStream().read());
 
-        // Version 2 read byte by byte and store each byte in array
-        byte[] bytes = new byte[14];
-        clientSocket.getInputStream().read(bytes);
-
-        // Display result using string class
-        System.out.println(new String(bytes));
-
-        // Version 3 read data as a string
+        // Read data from server
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String message = in.readLine();
 
         // Display result
         System.out.println(message);
+
+
+        // === Close All Connections === //
+
+
+        // Close OutputStreamWriter
+        out.close();
+
+        // Close BufferedReader
+        in.close();
 
         // Close client socket
         clientSocket.close();
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,35 +14,47 @@ public class Server {
         // Counter
         int count = 0;
 
+
         // === Establish server === //
+
 
         // Create Server Socket
         ServerSocket serverSocket = new ServerSocket(8000);
 
         // 5 connections
         while (count < 5) {
+
             // Create Client Socket
             Socket clientSocket = serverSocket.accept();
 
             // Counting how many clients reached the server
-            System.out.println("Connection no.:\t" + ++count);
+            System.out.println("Client no.:\t" + ++count);
+
+            // Read data as a string from client
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String request = in.readLine();
+            System.out.println(request);
+
 
             // === Create feedback === //
 
-           // Version 1
-           /*clientSocket.getOutputStream().write("<H2>Hello</H2>".getBytes());*/
 
-            // Version 2
+            // Write message to client
+            String response = "#" + count + ", your message length is\t" + request.length() + "\n";
             OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
-            out.write("\r\n<H2>From</H2>\t<H2>Server</H2>\tYou are client #" + count);
+            out.write(response);
             out.flush();
 
+
             // === Close All Connections === //
+
+            // Close BufferedReader
+            in.close();
 
             // Close OutputStreamWriter
             out.close();
 
-            // Close Client socket
+            // Close ServerSocket
             clientSocket.close();
         }
 
